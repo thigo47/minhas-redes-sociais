1cc7c3cd1b5c3d98419759537d6200ed20544521
thigo47 <vilaportilho@gmail.com>
2023-04-02 21:46:44 +0100

    img.src

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let divs = {
     insta:document.getElementById("insta"),
     twit:document.getElementById("twit"),
     yout:document.getElementById("yout"),
-    img:document.getElementById("img")
+    img:document.getElementById("img"),
+    iframe:document.getElementById("iframe")
 }
 
 divs.face.addEventListener("click",function(){
-    divs.img.src = ""                      
+    divs.iframe.src = "../facebook/index.html"
 })
